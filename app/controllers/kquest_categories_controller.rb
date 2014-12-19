7ac1094ed0794a4429f737f1efd4f6ee4f4bd490
Dominik Wei-Fieg <dominik.weifieg@gmail.com>
2014-12-19 10:19:49 +0100

    make reimports possible

--- app/controllers/kquest_categories_controller.rb
+++ app/controllers/kquest_categories_controller.rb
@@ -20,7 +20,7 @@ class KquestCategoriesController < ApplicationController
   def import(legacy_category, reimport)
     if(reimport)
       category = Category.find_by_old_uid_and_old_type(@category.cid, 'cat')
-      category.questions.clear
+      # category.questions.clear
       category.touch
     else
       category = Category.new(:title => legacy_category.cat_title, :description => legacy_category.cat_title, 
@@ -29,9 +29,11 @@ class KquestCategoriesController < ApplicationController
     
     legacy_category.kquest_questions.each do |legacy_question|
       next if legacy_question.deleted == 1
-      question = Question.new do |q|
-        q.old_type = 'cat'
-        q.old_uid = legacy_question.uid
+      question = Question.find_by_old_uid(legacy_question.uid)
+      if question
+        question.answers.clear
+        question.old_type = 'cat'
+        question.old_uid = legacy_question.uid
         body = legacy_question.question
         if(legacy_question.questiontype == 2)
           body << "\t\t<ol>"
@@ -47,13 +49,37 @@ class KquestCategoriesController < ApplicationController
           body << "\t\t\t<li>#{legacy_question.kombianswer_10}</li>" unless legacy_question.kombianswer_10.empty?
           body << "\t\t</ol>"
         end
-        q.body = body
+        question.body = body
         comment = legacy_question.commentedanswer
         #<link fileadmin/pdf-files_pro/herzinsuffizienz_klinik.pdf _blank download>siuhoisuh skdjhfgig asdf</link>
-        q.comment = comment.gsub(/<link (.*?) (.*?) (.*?)>(.*?)<\/link>/, '<a href="http://www.kreawi-online.de/\1" target="\2" class="\3">\4</a>') 
-        
+        question.comment = comment.gsub(/<link (.*?) (.*?) (.*?)>(.*?)<\/link>/, '<a href="http://www.kreawi-online.de/\1" target="\2" class="\3">\4</a>') 
+        category.questions << question unless category.questions.exists?(question)
+      else
+        question = Question.new do |q|
+          q.old_type = 'cat'
+          q.old_uid = legacy_question.uid
+          body = legacy_question.question
+          if(legacy_question.questiontype == 2)
+            body << "\t\t<ol>"
+            body << "\t\t\t<li>#{legacy_question.kombianswer_1}</li>" unless legacy_question.kombianswer_1.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_2}</li>" unless legacy_question.kombianswer_2.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_3}</li>" unless legacy_question.kombianswer_3.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_4}</li>" unless legacy_question.kombianswer_4.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_5}</li>" unless legacy_question.kombianswer_5.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_6}</li>" unless legacy_question.kombianswer_6.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_7}</li>" unless legacy_question.kombianswer_7.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_8}</li>" unless legacy_question.kombianswer_8.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_9}</li>" unless legacy_question.kombianswer_9.empty?
+            body << "\t\t\t<li>#{legacy_question.kombianswer_10}</li>" unless legacy_question.kombianswer_10.empty?
+            body << "\t\t</ol>"
+          end
+          q.body = body
+          comment = legacy_question.commentedanswer
+          #<link fileadmin/pdf-files_pro/herzinsuffizienz_klinik.pdf _blank download>siuhoisuh skdjhfgig asdf</link>
+          q.comment = comment.gsub(/<link (.*?) (.*?) (.*?)>(.*?)<\/link>/, '<a href="http://www.kreawi-online.de/\1" target="\2" class="\3">\4</a>') 
+        end
+        category.questions << question  
       end
-      category.questions << question
       question.save
       unless(legacy_question.questiontype == 3)
         a1 = Answer.new(:body => legacy_question.answer_a, :correct => (legacy_question.correct_answers | 1 == legacy_question.correct_answers))
